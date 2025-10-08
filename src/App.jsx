3ab6d780882bb708c91d8edb1da3be1bcc9e29ae
gbrds <joosep.holdt@gmail.com>
2025-10-08 12:32:09 +0300

    Code Finder logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import GameFrame from "./GameFrame";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import SoftDevGame from "./games/softDevGame/softDevGame.jsx";
 
 function App() {
   return (
-    <h1>Main page</h1>
+    <Router>
+      <Routes>
+        {/* Default redirect to the game */}
+        <Route path="/" element={<Navigate to="/softDevGame" replace />} />
+
+        {/* Main route for your minigame */}
+        <Route path="/softDevGame" element={<SoftDevGame />} />
+
+        {/* Fallback route */}
+        <Route path="*" element={<div>Page not found</div>} />
+      </Routes>
+    </Router>
   );
 }
 
